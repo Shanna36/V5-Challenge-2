af2f96c515d1418bd7c20e395a409acec5a5f2e0
Shelby Hanna <144790272+Shanna36@users.noreply.github.com>
2023-10-03 10:07:32 +0100

    fixed dog and ball

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float spawnCoolDown = 2.0f;
+
+    public float lastSpawnTime;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - lastSpawnTime >= spawnCoolDown)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastSpawnTime = Time.time;
         }
     }
 }
